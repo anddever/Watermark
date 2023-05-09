a2f76bf22034094f69aba06b3f0d51763e08127c
anddever <anddever@gmail.com>
2023-05-09 21:14:27 +0700

    Blending images

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,27 @@
-import domain.image.ImageData
-import java.io.File
-import kotlin.system.exitProcess
+import domain.image.ImageDataBuilder
+import domain.image.ImageValidator
+import domain.image.WatermarkGenerator
+import model.FileName
+import model.FileType
+import model.WatermarkWeight
 
 fun main() {
     println("Input the image filename:")
-    val filePath = readln()
-    val file = File(filePath)
-    if (file.exists()) {
-        val imageData = ImageData.Base(filePath, file)
-        imageData.printImageInfo()
-    } else {
-        print("The file $filePath doesn't exist.")
-        exitProcess(1)
-    }
+    val originalFilePath = readln()
+    val originalImage = ImageDataBuilder.Base(originalFilePath, FileType.IMAGE).build()
+
+    println("Input the watermark image filename:")
+    val watermarkFilePath = readln()
+    val watermarkImage = ImageDataBuilder.Base(watermarkFilePath, FileType.WATERMARK).build()
+
+    ImageValidator.Base(originalImage, FileType.IMAGE).compareDimensions(watermarkImage)
+
+    println("Input the watermark transparency percentage (Integer 0-100):")
+    val watermarkWeight = WatermarkWeight.Builder(readln()).build()
+
+    println("Input the output image filename (jpg or png extension):")
+    val outputFileName: FileName = FileName.Builder(readln()).build()
+
+    WatermarkGenerator.Base(originalImage, watermarkImage, watermarkWeight, outputFileName).saveWatermarkedImage()
+    println("The watermarked image ${outputFileName.name}.${outputFileName.extension.name.lowercase()} has been created.")
 }
